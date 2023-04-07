1b24244aa2b2bdfbe99991a7217f7c03c57b4785
Revan <tyler.arnold100@yahoo.com>
2023-04-07 13:42:13 -0400

    Added operations

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,11 @@
-# Prompt for User Input
+# User Input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+
+# Perform Operations
+print("Sum: {} + {} = {}".format(num1,num2,num1+num2))
+print("Difference: {} - {} = {}".format(num1,num2,num1-num2))
+print("Product: {} * {} = {}".format(num1,num2,num1*num2))
+print("Quotient: {} / {} = {}".format(num1,num2,num1/num2))
+print("Power: {} ** {} = {}".format(num1,num2,num1**num2))
+print("Remainder: {} % {} = {}".format(num1,num2,num1%num2))
