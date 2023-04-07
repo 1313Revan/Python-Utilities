89191337d9f61a0c0501a8b721810aa51c8d11cf
Revan <tyler.arnold100@yahoo.com>
2023-04-07 14:05:01 -0400

    Added operatons logic and choices

--- Calculator.py
+++ Calculator.py
@@ -2,10 +2,36 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
+print("""
+Choose an operation:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Exponent
+6. Division with Remainder
+""")
+
+op = int(input("Enter choice: "))
+
 # Perform Operations
-print("Sum: {} + {} = {}".format(num1,num2,num1+num2))
-print("Difference: {} - {} = {}".format(num1,num2,num1-num2))
-print("Product: {} * {} = {}".format(num1,num2,num1*num2))
-print("Quotient: {} / {} = {}".format(num1,num2,num1/num2))
-print("Power: {} ** {} = {}".format(num1,num2,num1**num2))
-print("Remainder: {} % {} = {}".format(num1,num2,num1%num2))
+if op == 1:
+    print("Sum: {} + {} = {}".format(num1,num2,num1+num2))
+elif op == 2:
+    print("Difference: {} - {} = {}".format(num1,num2,num1-num2))
+elif op == 3:
+    print("Product: {} * {} = {}".format(num1,num2,num1*num2))
+elif op == 4:
+    try:
+        print("Quotient: {} / {} = {}".format(num1,num2,num1/num2))
+    except:
+        print("Division by 0 not possible.")
+elif op == 5:
+    print("Power: {} ** {} = {}".format(num1,num2,num1**num2))
+elif op == 6:
+    try:
+        print("Quotient: {} / {} = {}, Remainder: {}".format(num1,num2,num1//num2,num1%num2))
+    except:
+        print("Division by 0 not possible.")
+else:
+    print("Please choose a valid option")
